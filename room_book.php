0dfb53dee4217ab2b151c04780e502688294a506
tueseed <tue_seed@hotmail.com>
2018-10-08 15:20:02 +0700

    Add files via upload

---
+++ room_book.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>จองห้องพัก-ไร่อิงภู</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," data-ajax="false" charset="utf-8">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+			
+	</head>
+	<body>
+		<div class="w3-container w3-purple">
+            <div class="w3-row">
+                <div class="w3-col w3-container m12 l12 w3-center">  
+                        <h1>จองห้องพัก-ไร่อิงภู</h1>
+                </div>
+            </div>
+        </div>
+		<div class="mt-5 container-fluid">
+            <div class="mt-5 row">
+                <div class="col-lg-4 offset-lg-4">
+                    <input type="date" class="form-control">
+                </div>
+            </div>
+        </div> 
+	</body>
+</html>
